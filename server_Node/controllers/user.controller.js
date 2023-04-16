ae0afb01fcb335fce339bc2e3f770885246c74cd
Bhawani_K <kundala.bhawani@outlook.com>
2023-04-16 15:52:27 +0530

    feature_Added getDetailsById

--- server_Node/controllers/user.controller.js
+++ server_Node/controllers/user.controller.js
@@ -1,13 +1,38 @@
 const express = require('express')
 const router = express.Router();
+const ObjectId = require('mongoose').Types.ObjectId;
+
 const UserModel = require("../models/user.model")
 
+// get All Records
 router.get('/register', (req, res) => {
     UserModel.find()
         .then(data => res.send(data))
         .catch(err => console.log(err))
 })
 
+// Get Details by Id
+router.get('/register/:id', (req, res) => {
+    if(ObjectId.isValid(req.params.id)== false){
+        res.status(400).json({
+            error : 'given object is not valid'
+        })
+    }else{
+        UserModel.findById(req.params.id)
+        .then(data => {
+            if(data){
+                res.send(data)
+            }else{
+                res.status(404).json({
+                    error:'No record with given _id :'+req.params.id
+                })
+            }
+        })
+        .catch(err => console.log(err))
+    }
+})
+
+// Create a Record
 router.post('/register', (req, res) => {
     UserModel.create(req.body)
     .then(data => res.status(201).json(data))
